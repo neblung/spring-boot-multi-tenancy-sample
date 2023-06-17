3165a314eda361bd35bf3099d93a47f24345bd58
Frank Neblung <frank.neblung@gmail.com>
2023-06-17 16:23:25 +0200

    Initial commit
    Generated by spring initializr

---
+++ src/main/kotlin/com/example/requestscopedemo/RequestscopedemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.requestscopedemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestscopedemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<RequestscopedemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/requestscopedemo/RequestscopedemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.requestscopedemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RequestscopedemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
